ec845d64c221b628e8ef5ce06e66b52b95fc968a
YONT23 <mendozaym01@gmail.com>
2022-03-07 16:34:27 -0500

    Clase-2-Taller
    Actualizacion

---
+++ Ejercicio4-Clase2.py
@@ -0,0 +1,30 @@
+from Library import *
+
+def main():
+    # Aplicacion de los Diccionarios
+  #  persona = {
+    #  "Nombre": "Yonatha"
+    #   "Apellido": "Mendoza"
+    #    "Edad": 23
+    #}
+
+ persona = {
+     "datospersonales":{
+    "Nombre": "Yonatha",
+       "Apellido": "Mendoza",
+        "Edad": 23
+     },
+     "salarial": {
+         "salario": 2000000,
+         "subtransporte": 50000,
+         "subalimentacion": 60000
+     }
+    }
+
+#print(persona["Nombre"]+ " " + persona["Apellido"])
+#persona["Nombre"] = "Molina"
+#print(f"{persona['Nombre']}{persona['Apellido']})
+#print(f"Nombre: {persona["datospersonales"]['nombre']}")
+
+if __name__ == "__main__":
+    main()
